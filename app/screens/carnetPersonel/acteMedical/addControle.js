68a2fd37b1ec61e225830a26e5554db20f42c6cc
aminaMakkaoui <48918184+aminaMakkaoui@users.noreply.github.com>
2020-07-08 21:18:29 +0100

    Update addControle.js

--- app/screens/carnetPersonel/acteMedical/addControle.js
+++ app/screens/carnetPersonel/acteMedical/addControle.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Button,StyleSheet, View, Text} from 'react-native';
 
-import TextInput from '../../../shared/TextInput';
+import {TextInput} from 'react-native-paper';
 import { globalStyles } from '../../../styles/global';
 
 import { Formik } from 'formik';
@@ -32,24 +32,28 @@ export default function AddControle({ addControle}){
              <View>
                 <TextInput
                label="Docteur"
+               mode='outlined' style={{marginTop: 10}} theme={{colors: {primary: '#6988cc', background: '#fff' }}}
                returnKeyType="next"
                onChangeText={props.handleChange('name')} value={props.values.name} onBlur={props.handleBlur('name')} />
               <Text style={globalStyles.errorText}>{props.touched.name && props.errors.name}</Text>
               
               <TextInput
                label="Specialite"
+               mode='outlined' style={{marginTop: 10}} theme={{colors: {primary: '#6988cc', background: '#fff' }}}
                returnKeyType="next"
                onChangeText={props.handleChange('specialite')} value={props.values.specialite} onBlur={props.handleBlur('specilite')} />
                <Text style={globalStyles.errorText}>{props.touched.specialite && props.errors.specialite}</Text>
 
                <TextInput
                label="Date"
+               mode='outlined' style={{marginTop: 10}} theme={{colors: {primary: '#6988cc', background: '#fff' }}}
                returnKeyType="next"
                onChangeText={props.handleChange('date')} value={props.values.date} onBlur={props.handleBlur('date')} />
                <Text style={globalStyles.errorText}>{props.touched.date && props.errors.date}</Text>
 
                <TextInput
                label="Date"
+               mode='outlined' style={{marginTop: 10}} theme={{colors: {primary: '#6988cc', background: '#fff' }}}
                returnKeyType="next"
                onChangeText={props.handleChange('remarque')} value={props.values.remarque} onBlur={props.handleBlur('remarque')} />
                <Text style={globalStyles.errorText}>{props.touched.remarque && props.errors.remarque}</Text>
