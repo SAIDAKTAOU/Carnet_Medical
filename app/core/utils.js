a64888e5a86b59c79ab7c00eb290571b4a55246a
aminaMakkaoui <48918184+aminaMakkaoui@users.noreply.github.com>
2020-07-24 02:39:03 +0100

    Update utils.js

--- app/core/utils.js
+++ app/core/utils.js
@@ -1,21 +1,20 @@
 export const emailValidator = email => {
-    const reg = /\S+@\S+\.\S+/;
-  
-    if (!email || email.length <= 0) return "Email cannot be empty.";
-    if (!reg.test(email)) return "Ooops! We need a valid email address.";
-  
-    return "";
-  };
-  
-  export const passwordValidator = password => {
-    if (!password || password.length <= 0) return "Password cannot be empty.";
-  
-    return "";
-  };
-  
-  export const nameValidator = name => {
-    if (!name || name.length <= 0) return "Name cannot be empty.";
-  
-    return "";
-  };
-  
+  const reg = /\S+@\S+\.\S+/;
+
+  if (!email || email.length <= 0) return "Champs d' Email ne peut pas etre vide.";
+  if (!reg.test(email)) return "L'adresse Email n'est pas valide.";
+
+  return "";
+};
+
+export const passwordValidator = password => {
+  if (!password || password.length <= 0) return "Champs du mot de passe ne peut pas etre vide.";
+
+  return "";
+};
+
+export const nameValidator = name => {
+  if (!name || name.length <= 0) return "Champs du nom ne peut pas etre vide.";
+
+  return "";
+};
