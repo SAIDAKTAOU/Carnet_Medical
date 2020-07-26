6af01f66fc75b5e6fb8745851c4ad13ed1ecf826
aminaMakkaoui <48918184+aminaMakkaoui@users.noreply.github.com>
2020-07-26 19:20:23 +0100

    Update HabitudesAlcool.js

--- app/screens/profilEtendu/HabitudesAlcool.js
+++ app/screens/profilEtendu/HabitudesAlcool.js
@@ -36,7 +36,7 @@ export default function HabitudeAlcool({ navigation }){
             </TouchableWithoutFeedback>
             </Modal>
             
-            <MaterialIcons name="add-box" size={28} color="#ff6666"  onPress={() => setModalOpen(true)} style={styles.modalToggle} />
+            <MaterialIcons name="add-box" size={28} color="#9159b8"  onPress={() => setModalOpen(true)} style={styles.modalToggle} />
 {/*     <MaterialIcons name='add' size={28} onPress={() => setModalOpen(true)} style={styles.modalToggle} />
       */}     
                <DataTable style={styles.datatable}>
